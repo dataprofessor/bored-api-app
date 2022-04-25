49c518a8f1c52b1ea9344c9f8ca520c4a5768324
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-25 23:32:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,11 @@ st.title('🏀 Bored API app')
 
 selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
 
-st.header('Suggested activity')
+st.header('JSON data')
 suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
 json_data = requests.get(suggested_activity_url)
 suggested_activity = json_data.json()
 st.write(suggested_activity)
+
+st.header('Suggested activity')
+st.write('Suggested activity', suggested_activity['activity'])
