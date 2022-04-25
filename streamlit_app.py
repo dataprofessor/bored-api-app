92bb1cb89f1efb180d166c316f8a6d4e6f161e0b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-25 23:47:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import requests
+import numpy as np
 
 st.title('🏀 Bored API app')
 
 selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+selected_participants = st.sidebar.slider('Select the number of participants', np.arange(1,10,1))
 
 st.header('JSON data')
-suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}&participants={selected_participants}'
 json_data = requests.get(suggested_activity_url)
 suggested_activity = json_data.json()
 st.write(suggested_activity)
 
 st.header('Suggested activity')
 st.info(suggested_activity['activity'])
+st.metric('Number of Participants', selected_participants)
