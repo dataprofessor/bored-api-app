cc9d949bc4330e094517003fec8ca9bbeeffbe58
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-25 18:51:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as st
 
+st.title('🏀 Activities to Do')
+
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+
+st.header('Suggested activity')
+suggested_activity = f'http://www.boredapi.com/api/activity?type={selected_type}'
+st.info(suggested_activity)
