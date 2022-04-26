c73dd68d9e487ade0b87d0796f43b4f32055d1c9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-26 09:47:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.sidebar.header('Input')
 selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
 selected_participants = st.sidebar.slider('Select number of participants', 1, 10, 1)
 
-suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}?participants={selected_participants}'
 json_data = requests.get(suggested_activity_url)
 suggested_activity = json_data.json()
 
