4eb8a7ea29330980d582c1aa478c282bee031dce
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-26 09:37:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ st.header('Suggested activity')
 st.info(suggested_activity['activity'])
 
 col1, col2, col3 = st.columns(3)
-with col1:
-  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+#with col1:
+#  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
 with col2:
   st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
 with col3:
